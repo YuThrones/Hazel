1c23511715c30521afece7e9d63bba75c1bc9cab
YuThrones <YuThrones@users.noreply.github.com>
2023-11-28 11:35:10 +0800

    P7 引入Premake工具

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -7,6 +7,7 @@ extern Hazel::Application* Hazel::CreateApplication();
 int main(int argc, char** argv)
 {
 	Hazel::Log::Init();
+	HZ_CORE_INFO("Welcome to Hazel Engine!");
 
 	auto app = Hazel::CreateApplication();
 	app->Run();
