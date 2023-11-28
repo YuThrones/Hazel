1a0dea4e8601ea0ed940cdfa33ac270d9f804aeb
YuThrones <YuThrones@users.noreply.github.com>
2023-11-28 10:15:50 +0800

    P6 日志系统

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -2,8 +2,8 @@
 
 // For use by Hazel Applications
 
-#include <stdio.h>
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // ----------Hazel Entry Point ---------------
 #include "Hazel/EntryPoint.h"
